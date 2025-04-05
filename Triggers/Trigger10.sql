b1ea568f2d101b7767cc669613390f24f218ed7f
Kiet Lam (Brian) <l.vikiet21@gmail.com>
2025-04-05 18:51:41 +0700

    Add files via upload

---
+++ Triggers/Trigger10.sql
@@ -0,0 +1,33 @@
+﻿ -- Phone numbers must consist of exactly 10 digits.
+ --Employees
+ CREATE TRIGGER tr_sdtnv on NHAN_VIEN
+ AFTER INSERT, UPDATE
+ AS DECLARE @sdtnv CHAR(20);
+ SELECT @sdtnv = sdt FROM INSERTED
+ IF LEN(@sdtnv) <> 10
+ BEGIN
+	PRINT(N'Số điện thoại nhập vào không hợp lệ');
+	ROLLBACK TRANSACTION;
+ END
+
+ --Customers
+ CREATE TRIGGER tr_sdtkh on KHACH_HANG
+ AFTER INSERT, UPDATE
+ AS DECLARE @sdtkh CHAR(20);
+ SELECT @sdtkh = sdt FROM INSERTED
+ IF LEN(@sdtkh) <> 10
+ BEGIN
+	PRINT(N'Số điện thoại nhập vào không hợp lệ');
+	ROLLBACK TRANSACTION;
+ END
+
+ --Suppliers
+ CREATE TRIGGER tr_sdtncc on NHA_CUNG_CAP
+ AFTER INSERT, UPDATE
+ AS DECLARE @sdtncc CHAR(20);
+ SELECT @sdtncc = sdt FROM INSERTED
+ IF LEN(@sdtncc) <> 10
+ BEGIN
+	PRINT(N'Số điện thoại nhập vào không hợp lệ');
+	ROLLBACK TRANSACTION;
+ END
---
+++ Triggers/Trigger9.sql
@@ -0,0 +1,9 @@
+﻿ CREATE TRIGGER tr_tendv ONHANG_HOA
+ AFTER INSERT, UPDATE
+ AS DECLARE @dvt NVARCHAR(15);
+ SELECT @dvt = donvitinh FROM INSERTED
+ IF EXISTS(SELECT 1 FROM INSERTED WHERE donvitinh NOT IN(N'Cái',N'Cặp',N'Bộ')
+ BEGIN
+	PRINT(N'Ngày nhập hàng không được lớn hơn ngày hiện tại');
+	ROLLBACK TRANSACTION;
+ END
