80d7421e8a3f3c4791172ba2c2f4036c3e5e6423
Kiet Lam (Brian) <l.vikiet21@gmail.com>
2025-04-05 18:33:41 +0700

    Update Trigger5.sql

--- Triggers/Trigger5.sql
+++ Triggers/Trigger5.sql
@@ -1,4 +1,5 @@
-﻿CREATE TRIGGER nv_nhap ON PHIEU_NHAP
+--Import receipt restriction
+CREATE TRIGGER nv_nhap ON PHIEU_NHAP
 AFTER INSERT, UPDATE
 AS
 DECLARE @manvn CHAR(5);
@@ -9,3 +10,15 @@ BEGIN
 	PRINT(N'Phải là nhân viên nhập kho')
 	ROLLBACK TRANSACTION
 END
+--Export receipt restriction
+ CREATE TRIGGER nv_xuat ON PHIEU_XUAT
+ AFTER INSERT, UPDATE
+ AS
+ DECLARE @manv CHAR(5);
+ SELECT @manv = manv from inserted
+ IF NOT EXISTS (SELECT * FROM CONG_VIEC CV join NHAN_VIEN NV
+ 	ON CV.MACV = NV.MACV and CV.TENCV = N'Xuất kho' Join PHIEU_XUAT PX ON NV.manv = PX.manv where @manv = NV.MANV)
+ BEGIN
+ 	PRINT(N'Phải là nhân viên xuất kho')
+ 	ROLLBACK TRANSACTION
+ END
