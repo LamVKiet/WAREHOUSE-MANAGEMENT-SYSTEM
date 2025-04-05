e7b8017d045e6131e52423cbca5fe658eccde2a4
Kiet Lam (Brian) <l.vikiet21@gmail.com>
2025-04-05 18:40:35 +0700

    Add files via upload

---
+++ Triggers/Trigger6.sql
@@ -0,0 +1,20 @@
+﻿ --Import and export dates must not be later than the current date.
+ CREATE TRIGGER tr_ngaynhap ON PHIEU_NHAP
+ AFTER INSERT, UPDATE
+ AS DECLARE @nn DATE;
+ SELECT @nn = ngaynhap FROM INSERTED
+ IF @nn > GETDATE()
+ BEGIN
+	PRINT(N'Ngày nhập hàng không được lớn hơn ngày hiện tại');
+	ROLLBACK TRANSACTION;
+ END
+
+ CREATE TRIGGER tr_ngayxuat ON PHIEU_XUAT
+ AFTER INSERT, UPDATE
+ AS DECLARE @nx DATE;
+ SELECT @nx = ngayxuat FROM INSERTED
+ IF @nx > GETDATE()
+ BEGIN
+	PRINT(N'Ngày xuất hàng không được lớn hơn ngày hiện tại');
+	ROLLBACK TRANSACTION;
+ END
