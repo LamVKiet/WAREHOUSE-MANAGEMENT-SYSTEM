3e8ba073315a0175342dc1165e6dcbd1ee2e9540
Kiet Lam (Brian) <l.vikiet21@gmail.com>
2025-04-05 18:43:49 +0700

    Add files via upload

---
+++ Triggers/Trigger8.sql
@@ -0,0 +1,10 @@
+﻿CREATE TRIGGER tr_tcv ON CONG_VIEC
+ AFTER INSERT, UPDATE
+ AS DECLARE @tencv NVARCHAR(10);
+ SELECT @tencv = tencv FROM INSERTED
+ IF EXISTS (SELECT 1 FROM INSERTED
+ WHERE tencv NOT IN (N'An ninh', N'Xuất kho', N'Nhập kho', N'Quản lí',  N'Kĩ thuật', N'Dịch vụ')
+ BEGIN
+	PRINT(N'Tên công việc không hợp lệ');
+	ROLLBACK TRANSACTION;
+ END
